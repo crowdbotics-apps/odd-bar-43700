7e7880e650a4b9e430de696cae04ee4646af9d8b
Crowdbotics <team@crowdbotics.com>
2023-10-02 01:23:38 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "odd-bar-43700",
+  url: "https://odd-bar-43700.botics.co",
+  api: "https://odd-bar-43700.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
